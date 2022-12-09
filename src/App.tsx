71c1b77b9cd44a218846ad88eb7807477a84aba3
Alexandre Bekor <staff@agenciabekor.com>
2022-12-09 11:09:43 -0300

    :star: initialize project

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import { Header } from "./components/Header"
+import './globals.css'
+
+const App = () => {
+  return (
+    <div>
+      <Header />
+      <p>Hello World</p>
+    </div>
+  )
+}
+
+export default App
---
+++ src/components/Header.tsx
@@ -0,0 +1,8 @@
+import styles from './Header.module.css'
+
+export const Header = () => {
+  console.log(styles)
+  return (
+    <header className={styles.header}>Teste</header>
+  )
+}
